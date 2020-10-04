28c4d5f5380af87f004316f80f5b27e7dd8ebdab
pacokwon <haechank@gmail.com>
2020-10-04 22:02:21 +0900

    add(hw2): organize code for imfilter

--- assignment2/code/my_imfilter.m
+++ assignment2/code/my_imfilter.m
@@ -1,7 +1,7 @@
 function output = my_imfilter(image, filter)
 % This function is intended to behave like the built in function imfilter()
-% when operating in convolution mode. See 'help imfilter'. 
-% While "correlation" and "convolution" are both called filtering, 
+% when operating in convolution mode. See 'help imfilter'.
+% While "correlation" and "convolution" are both called filtering,
 % there is a difference. From 'help filter2':
 %    2-D correlation is related to 2-D convolution by a 180 degree rotation
 %    of the filter matrix.
@@ -10,30 +10,29 @@ function output = my_imfilter(image, filter)
 
 % Boundary handling can be tricky as the filter can't be centered on pixels
 % at the image boundary without parts of the filter being out of bounds. If
-% we look at 'help imfilter', we see that there are several options to deal 
-% with boundaries. 
+% we look at 'help imfilter', we see that there are several options to deal
+% with boundaries.
 % Please recreate the default behavior of imfilter:
-% to pad the input image with zeros, and return a filtered image which matches 
-% the input image resolution. 
+% to pad the input image with zeros, and return a filtered image which matches
+% the input image resolution.
 % A better approach is to mirror or reflect the image content in the padding.
 
 % Uncomment to call imfilter to see the desired behavior.
 %     output = imfilter(image, filter, 'conv');
 
     % rotate filter first
-    rotated_filter = rot90(filter, 2);
-    [filter_m, filter_n] = size(rotated_filter);
-    
+    [filter_m, filter_n] = size(filter);
+
     % check filter dimension
-    if mod(filter_m, 2) == 0 && mod(filter_n, 2) == 0
-        output = "Filter must be odd";
-        return
+    if mod(filter_m, 2) == 0 || mod(filter_n, 2) == 0
+        error("Filter size must be odd");
     end
     half_m = (filter_m - 1) / 2;
     half_n = (filter_n - 1) / 2;
-    
+
     output = zeros(size(image));
-    
+    rotated_filter = rot90(filter, 2);
+
     % pad image with zeros
     image = padarray(image, [half_m, half_n], 'symmetric', 'both');
     [m, n, h] = size(image);
