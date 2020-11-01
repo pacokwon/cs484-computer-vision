7d9685a2ae9780a635974752babc112c5751a9bc
pacokwon <haechank@gmail.com>
2020-11-01 21:40:56 +0900

    update: disparity map calculation method

--- assignment3/hw3_2020f/code/calculate_disparity_map.m
+++ assignment3/hw3_2020f/code/calculate_disparity_map.m
@@ -8,30 +8,25 @@ function d = calculate_disparity_map(img_left, img_right, window_size, max_dispa
     left_img = img_left;
     right_img = img_right;
 
-    % subtract mean
-    left_average = mean(left_img,'all');
-    right_average = mean(right_img, 'all');
-    
-    disp(size(left_img));
-    
-    left_img = left_img - left_average;
-    right_img = right_img - right_average;
-
-    %% Delete here when you run your code
-
     window_half = (window_size - 1) / 2;
 
     cost_vol = zeros(size(img_left));
     for h=window_half + 1:height-window_half
+        fprintf("[%d]\n", h);
     for w=window_half + 1:width-window_half
         % for every pixel
-        fprintf("[%d %d]\n", h, w);
-        minimum_cost = max_disparity;
+        minimum_cost = -1;
         minimum_spot = 0;
         right_window = right_img(h - window_half : h + window_half, w - window_half : w + window_half); % fixed right window
-        for y=window_half + 1:w - window_half
+        right_window = right_window - mean(right_window, 'all');
+        for y=1:max_disparity
+            if w + y + window_half > width
+                continue
+            end
+
             % search every pixel/window in left image
-            left_window = left_img(h - window_half : h + window_half, y - window_half : y + window_half); % variable left window
+            left_window = left_img(h - window_half : h + window_half, w + y - window_half : w + y + window_half); % variable left window
+            left_window = left_window - mean(left_window, 'all');
 
             numerator = sum(left_window .* right_window, 'all');
             denominator = sqrt(sum(left_window .* left_window, 'all')) * sqrt(sum(right_window .* right_window, 'all'));
@@ -39,18 +34,16 @@ function d = calculate_disparity_map(img_left, img_right, window_size, max_dispa
             cost = numerator / denominator;
 
             % update minimum cost
-            if minimum_cost > cost
+            if minimum_cost < cost
                 minimum_cost = cost;
                 minimum_spot = y;
             end
         end
 
         % set minimum cost
-        cost_vol(h, w) = w - minimum_spot;
+        cost_vol(h, w) = minimum_spot;
     end
     end
     scaling_factor = 255 / max_disparity;
     d = cost_vol * scaling_factor;
-    % winner takes all
-%     [min_val, d] = max(cost_vol, [], 3);
 end
--- assignment3/hw3_2020f/code/calculate_fundamental_matrix.m
+++ assignment3/hw3_2020f/code/calculate_fundamental_matrix.m
@@ -4,28 +4,24 @@
 function f = calculate_fundamental_matrix(pts1, pts2)
     rows = size(pts1, 1);
 
-    pts1h = [pts1, ones(rows, 1)]';
-    pts2h = [pts2, ones(rows, 1)]';
+    pts1 = [pts1, ones(rows, 1)]';
+    pts2 = [pts2, ones(rows, 1)]';
 
-    [pts1, t1] = normalize_points(pts1h, 2);
-    [pts2, t2] = normalize_points(pts2h, 2);
+    [pts1, t1] = normalize_points(pts1, 2);
+    [pts2, t2] = normalize_points(pts2, 2);
 
-    A = zeros(rows, 9);
+    A = zeros(rows, 9, 'double');
     for idx = 1: rows
       A(idx,:) = [ pts1(1,idx) * pts2(1,idx), pts1(2,idx) * pts2(1,idx), pts2(1,idx), pts1(1,idx) * pts2(2,idx), pts1(2,idx) * pts2(2,idx), pts2(2,idx), pts1(1,idx), pts1(2,idx), 1 ];
     end
 
-    % Find out the eigen-vector corresponding to the smallest eigen-value.
+    % Extract eigenvector corresponding to least singular value
     [~, ~, V] = svd(A, 0);
+    % rearrange 9 entries of V(:,end)
     F = reshape(V(:, end), 3, 3)';
 
-    % Enforce rank-2 constraint
     [U, S, V] = svd(F);
     F = U * diag([S(1, 1) S(2, 2) 0]) * V';
-
-    % Transform the fundamental matrix back to its original scale.
     F = t2' * F * t1;
-
-    % Normalize the fundamental matrix.
     f = F / norm(F);
 end
