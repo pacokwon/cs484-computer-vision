ff84d2ed4e10ba0788d743eaca5a0db5ed0ea901
pacokwon <haechank@gmail.com>
2020-11-01 21:39:28 +0900

    update: padding for stereo rectification

--- assignment3/hw3_2020f/code/rectify_stereo_images.m
+++ assignment3/hw3_2020f/code/rectify_stereo_images.m
@@ -2,7 +2,6 @@
 % Given two homography matrices for two images, generate the rectified
 % image pair.
 function [rectified1, rectified2] = rectify_stereo_images(img1, img2, h1, h2)
-
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Your code here
 
@@ -13,6 +12,25 @@ function [rectified1, rectified2] = rectify_stereo_images(img1, img2, h1, h2)
     transform1 = projective2d(h1);
     transform2 = projective2d(h2);
 
-    rectified1 = imwarp(img1, transform1, 'OutputView', imref2d(size(img1)));
-    rectified2 = imwarp(img2, transform2, 'OutputView', imref2d(size(img2)));
+    [height, width, ~] = size(img1);
+    corners = [1, 1; 1, height; width, height; width, 1];
+    img1Corners = transformPointsForward(transform1, corners);
+    img2Corners = transformPointsForward(transform2, corners);
+    transformedCorners = [img1Corners; img2Corners];
+
+    xMax = max(transformedCorners(:,1));
+    xMin = min(transformedCorners(:,1));
+
+    yMax = max(transformedCorners(:,2));
+    yMin = min(transformedCorners(:,2));
+
+    paddedWidth = round(xMax - xMin);
+    paddedHeight = round(yMax - yMin);
+    xWorldLimits = [xMin, xMax];
+    yWorldLimits = [yMin, yMax];
+
+    imref = imref2d([paddedHeight, paddedWidth], xWorldLimits, yWorldLimits);
+
+    rectified1 = imwarp(img1, transform1, 'OutputView', imref);
+    rectified2 = imwarp(img2, transform2, 'OutputView', imref);
 end
